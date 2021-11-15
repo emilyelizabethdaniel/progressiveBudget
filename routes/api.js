81608318cd7b3fd07dfe930d297b5eb97a572164
Basquiat <ememdaniel98@gmail.com>
2021-11-15 12:03:50 -0600

    finished routes

--- routes/api.js
+++ routes/api.js
@@ -10,5 +10,24 @@ router.post("/api/transaction", ({body}, res) => {
         res.status(404).json(err);
       });
   });
+  router.post("/api/transaction/bulk", ({body}, res) => {
+    Transaction.insertMany(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
+  
+  router.get("/api/transaction", (req, res) => {
+    Transaction.find({}).sort({date: -1})
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
 
 module.exports = router;
