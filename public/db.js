d26dea55f788148a8e6a83d5e7a393e315e04a1a
Basquiat <ememdaniel98@gmail.com>
2021-11-15 14:57:18 -0600

    front end js, ss

--- public/db.js
+++ public/db.js
@@ -1,4 +1,3 @@
-// Different browsers can have different names for the indexedDB object, so we standardize that here
 const indexedDB =
   window.indexedDB ||
   window.mozIndexedDB ||
@@ -8,44 +7,37 @@ const indexedDB =
 
 let db;
 
-// Tell indexedDb to open (or create) whatever database you want to work with
-// I'd recommend naming the database something like "budget" and the object store 
-// something like "pending" or "tba"
-const request = indexedDB.open("<your db name here>", 1);
+const request = indexedDB.open("budget", 1);
 
-// Set up your object store
-// Think of an object store as a table inside your database
 request.onupgradeneeded = ({ target }) => {
   let db = target.result;
-  db.createObjectStore("<object store name here>", { autoIncrement: true });
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
-// Leave this code as-is
-// If the request was successful it means the Internet is back up, so we can query the real database.
+
 request.onsuccess = ({ target }) => {
   db = target.result;
-  // check if app is online before reading from db
+
   if (navigator.onLine) {
     checkDatabase();
   }
 };
 
-// Simple error handler. Leave as-is
 request.onerror = function(event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
-// This function is called when it's time to save data to the indexedDb
+
 function saveRecord(record) {
-  const transaction = db.transaction(["<object store name here>"], "readwrite");
-  const store = transaction.objectStore("<object store name here>");
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
   store.add(record);
 }
 
-// This function runs when we detect that the internet connection is working again. It sends a post request to the server with all the saved data so that the data can be synced with the server, and then it wipes out the existing indexedDb. You can keep as-is, unless you want to change the name of the fetch route.
+
 function checkDatabase() {
-  const transaction = db.transaction(["<object store name here>"], "readwrite");
-  const store = transaction.objectStore("<object store name here>");
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
   const getAll = store.getAll();
 
   getAll.onsuccess = function() {
@@ -62,9 +54,9 @@ function checkDatabase() {
         return response.json();
       })
       .then(() => {
-        // delete records if successful
-        const transaction = db.transaction(["<object store name here>"], "readwrite");
-        const store = transaction.objectStore("<object store name here>");
+
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
         store.clear();
       });
     }
